758d882af4f767052ff600847d50ee0776d54810
Susie Coleman <susie.coleman@bbc.co.uk>
2020-05-11 15:12:59 +0100

    feat: Add CORS headers to Languages endpoint

--- languages-lambda/index.js
+++ languages-lambda/index.js
@@ -1,9 +1,14 @@
-const languagePairs = require("./languagePairs")
+const languagePairs = require("./languagePairs");
 
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(languagePairs)
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(languagePairs),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    },
+  };
+  return response;
 };
